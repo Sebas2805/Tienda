90109cd00856c1f43adcf7d8162dd629c7691761
Sebastian <121842634+Sebas2805@users.noreply.github.com>
2025-07-13 00:34:35 -0600

    Semana 09

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -5,6 +5,7 @@
 package com.tienda.dao;
 
 import com.tienda.domain.Categoria;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -13,4 +14,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface CategoriaDao extends JpaRepository<Categoria, Long>{
     //TODO> Crear metodos ampliados
+    
+    List<Categoria> findByDescripcionContainingIgnoreCase (String descripcion);
 }
